faca0c530d720df1a9362bfa92a1cf4ef4e3fde2
Vasili Gulevich <V.V.Gulevich@mail.ru>
2014-12-28 01:50:35 +0600

    Implemented hudson job links
    Links are not updated on internal gerrit navigation.

--- Gerrit_Jira_backreferences.user.js
+++ Gerrit_Jira_backreferences.user.js
@@ -3,13 +3,20 @@
 // @namespace   xored
 // @description Adds backlinks from Gerrit to JIRA
 // @include     https://git.eclipse.org/*
-// @version     1
+// @include		https://hudson.eclipse.org/rcptt/*job/rcptt-all-gerrit/
+// @version     2
 // @grant       GM_getValue
 // @grant       GM_setValue
 // @grant       GM_log
 // ==/UserScript==
 
 //GM_log = function (data) {};
+
+var gerritHrefReg = /https:\/\/git.eclipse.org\/r\/#\/c\/(\d+)\//;
+var changeInfoXpath = './/table[@class="infoBlock changeInfoBlock"]/tbody';
+var jiraLinkReg = /https:\/\/[\w\.]+\/browse\/([^\/ #\?]+)(\/.*)?/;
+var hudsonLinkReg = /https:\/\/(.*)(?:\/view\/\w+)?\/job\/([\w\-]+)\/(\d+)\/?/;
+
 
 function deserialize(name, def) {
 	return eval(GM_getValue(name, (def || '({})')));
@@ -19,9 +26,11 @@ function serialize(name, val) {
 	GM_setValue(name, uneval(val));
 }
 
-function getNodes(expression) {
+function getNodes(expression, parent) {
 	GM_log("Looking for " + expression);
-	var i = document.evaluate(expression, document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE , null);
+	if (!parent)
+		parent = document;
+	var i = document.evaluate(expression, parent, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE , null);
     if (!i)
         throw new Error("Invalid query: "+expression);
 	var rv = [];
@@ -29,6 +38,7 @@ function getNodes(expression) {
 		GM_log("Node");
 		rv.push(data);
     }
+	GM_log("Found " + rv.length + " nodes");
 	return rv;
 }
 
@@ -38,25 +48,12 @@ function getOnly(array) {
 	return array[0];
 }
 
-function getNode(expression) {
-	var array = getNodes(expression);
+function getNode(expression, parent) {
+	var array = getNodes(expression, parent);
 	if (array.length != 1)
-		throw new Error("Array contains " + array.length + " elements:" + array);
+		throw new Error("There are " + array.length + " elements of "+expression+" :" + array);
 	return array[0];
 }
-
-var gerritHrefReg = /https:\/\/git.eclipse.org\/r\/#\/c\/(\d+)\//;
-var changeInfoXpath = './/table[@class="infoBlock changeInfoBlock"]/tbody';
-var jiraLinkReg = /https:\/\/[\w\.]+\/browse\/([^\/ #\?]+)(\/.*)?/;
-
-GM_log("Location: " + document.location + ", referer: " + document.referrer);
-
-var components =  gerritHrefReg.exec(""+location.href);
-if (!components) {
-	throw new Error("Can't find git change id in " + location.href);
-}
-
-var gerritId = components[1];
 
 function getIssueNumberFromLink(link) {
 	var res = jiraLinkReg.exec(link);
@@ -65,23 +62,94 @@ function getIssueNumberFromLink(link) {
 	return null;
 }
 
-var jiraLinksKey = "jiraLinks";
-var jiraLinks = deserialize(jiraLinksKey, {});
-if (getIssueNumberFromLink(document.referrer)) {
-	jiraLinks[gerritId] = document.referrer;
-	serialize(jiraLinksKey, jiraLinks);
+GM_log("Location: " + document.location + ", referer: " + document.referrer + ", normalized gerrit: " +  normalizeGerritUrl(location.href) );
+
+var hudsonLinks = deserialize("hudsonLinks", {});
+
+if (normalizeGerritUrl(location.href)) {
+	var jiraLinks = deserialize("jiraLinks", {});
+	var gerritUrl = normalizeGerritUrl(location.href);
+	if (getIssueNumberFromLink(document.referrer)) {
+		jiraLinks[normalizeGerritUrl(location.href)] = document.referrer;
+		serialize("jiraLinks", jiraLinks);
+	}
+	var jiraLink = jiraLinks[gerritUrl];
+	if (jiraLink) {
+		GM_log("JIRA link: " + jiraLink);
+		window.addEventListener('load', function() {
+			extendChangeInformation("JIRA", getIssueNumberFromLink(jiraLink), jiraLink);
+		});
+	}
+	if (gerritUrl) {
+		var hudsonLink = hudsonLinks[gerritUrl];
+		GM_log("Hudson link: " + hudsonLink);
+		if (hudsonLink)
+			window.addEventListener('load', function() {
+				extendChangeInformation("Hudson", extractJobId(hudsonLink), hudsonLink);
+			});
+	}
+} else if (document.location.href.contains("/job/")) {
+	window.addEventListener('load', function() {
+		var log = "";
+		var present = {};
+		extractBuildHistory(function (hudsonUrl, gerritUrl) {
+			if (present[gerritUrl])
+				return;
+			present[gerritUrl] = true;
+			gerritUrl = normalizeGerritUrl(gerritUrl);
+			hudsonLinks[gerritUrl] = hudsonUrl;
+			log+=(gerritUrl + " : " + hudsonUrl + "\n");
+		});
+		serialize("hudsonLinks", hudsonLinks);
+		GM_log("Found history: \n" + log);
+	});
+}
+
+function extractJobId(hudsonUrl) {
+	var res = hudsonLinkReg.exec(hudsonUrl)
+	if (!res) 
+		throw new Error(hudsonUrl + " is not a hudson link");
+	return res[2] + "/" + res[3];
+}
+
+function normalizeGerritUrl(gerritUrl) {
+	//Normal form: https://git.eclipse.org/r/38663
+	//Final form: https://git.eclipse.org/r/#/c/38105/ 
+	var exp = /https:\/\/([\w\.\-]+)\/r(?:\/#\/c)?\/(\d+)\/?/;
+	var res = exp.exec(gerritUrl);
+	if (!res)
+		return null;
+	return "https://"+res[1]+"/r/" + res[2] + "/";
 }
 
 
-function extendChangeInformation(jiraLink) {
+
+function extractBuildHistory(callback) {
+	var rows = getNodes("id('buildHistory')//tr[contains(@class, 'build-row no-wrap')]");
+	for(var i in rows) {
+		var row = rows[i];
+		var hudsonUrl = getNode("td[2]/a", row).href;
+		var gerritCell = getNode("td[4]/a", row);
+		var gerritUrl = gerritCell.href;
+		callback(hudsonUrl, gerritUrl);
+	}
+	return history;	
+}
+
+function addJiraLink(jiraLink) {
+	return extendChangeInformation("JIRA", getIssueNumberFromLink(jiraLink), jiraLink);
+}
+
+function extendChangeInformation(field, name, link) {
 	var infoTable = getNode(changeInfoXpath);
 	var row = document.createElement("tr");
 	var issue = getIssueNumberFromLink(jiraLink);
-	row.innerHTML = '<td class="header">JIRA</td><td><a href="'+jiraLink +'">'+issue+'</a></td>';
+	if (name && link)
+		value = '<a href="'+link +'">'+name+'</a>';
+	else 
+		value = 'None';
+	row.innerHTML = '<td class="header">'+field+'</td><td>'+value+'</td>';
+
 	infoTable.appendChild(row);
 }
 
-if (jiraLinks[gerritId])
-	window.addEventListener('load', function() {extendChangeInformation(jiraLinks[gerritId]);});
-
-//window.setTimeout(extendChangeInformation, 3000);
