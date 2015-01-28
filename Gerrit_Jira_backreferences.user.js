fb56a5247d696304ba97a4b8e9165f53f776a21f
Vasili Gulevich <V.V.Gulevich@mail.ru>
2015-01-28 23:17:03 +0600

    Using extracted utils

--- Gerrit_Jira_backreferences.user.js
+++ Gerrit_Jira_backreferences.user.js
@@ -4,7 +4,8 @@
 // @description Adds backlinks from Gerrit to JIRA
 // @include     https://git.eclipse.org/*
 // @include		https://hudson.eclipse.org/rcptt/*job/rcptt-all-gerrit/
-// @version     4
+// @require		https://github.com/basilevs/jiralinks/raw/master/utils.js
+// @version     5
 // @grant       GM_getValue
 // @grant       GM_setValue
 // @grant       GM_log
@@ -15,44 +16,6 @@
 var changeInfoXpath = './/table[@class="infoBlock changeInfoBlock"]/tbody';
 var jiraLinkReg = /https:\/\/[\w\.]+\/browse\/([^\/ #\?]+)(\/.*)?/;
 var hudsonLinkReg = /https:\/\/(.*)(?:\/view\/\w+)?\/job\/([\w\-]+)\/(\d+)\/?/;
-
-
-function deserialize(name, def) {
-	return eval(GM_getValue(name, (def || '({})')));
-}
-
-function serialize(name, val) {
-	GM_setValue(name, uneval(val));
-}
-
-function getNodes(expression, parent) {
-	GM_log("Looking for " + expression);
-	if (!parent)
-		parent = document;
-	var i = document.evaluate(expression, parent, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE , null);
-    if (!i)
-        throw new Error("Invalid query: "+expression);
-	var rv = [];
-    while (data = i.iterateNext()) {
-		GM_log("Node");
-		rv.push(data);
-    }
-	GM_log("Found " + rv.length + " nodes");
-	return rv;
-}
-
-function getOnly(array) {
-	if (array.length != 1)
-		throw new Error("Array contains " + array.length + " elements:" + array);
-	return array[0];
-}
-
-function getNode(expression, parent) {
-	var array = getNodes(expression, parent);
-	if (array.length != 1)
-		throw new Error("There are " + array.length + " elements of "+parent.outerHTML.substr(0, 500)+" :" + array);
-	return array[0];
-}
 
 function getIssueNumberFromLink(link) {
 	var res = jiraLinkReg.exec(link);
