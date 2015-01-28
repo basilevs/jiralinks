e1a41c4ecb3bd2b5e4435f47a61e16881c14e446
Vasili Gulevich <V.V.Gulevich@mail.ru>
2015-01-29 00:04:26 +0600

    Added a check for invalid page.

--- test_results.js
+++ test_results.js
@@ -12,11 +12,15 @@
 var link = getNode("(id('left-top-nav')/a)[last()]");
 
 var buildNum = parseInt(link.textContent.replace(/#/g, ""), 10);
-var url = link.href.replace("lastBuild", ""+buildNum);
-var tasks = document.getElementById('tasks');
+if (!isNaN(buildNum)) {
+	var url = link.href.replace("lastBuild", ""+buildNum);
+	var tasks = document.getElementById('tasks');
 
-if (getNodes("//table[contains(@class, 'progress-bar')]").length == 0) {
-	var div = document.createElement("div");
-	div.innerHTML = "<img style='margin: 2px;' src='/rcptt/static/f580e051/images/24x24/clipboard.png'> <a href='" + url +"artifact/rcpttTests/target/results/tests.html'>RCPTT Report</a>";
-	tasks.appendChild(div);
+	if (getNodes("//table[contains(@class, 'progress-bar')]").length == 0) {
+		var div = document.createElement("div");
+		div.innerHTML = "<img style='margin: 2px;' src='/rcptt/static/f580e051/images/24x24/clipboard.png'> <a href='" + url +"artifact/rcpttTests/target/results/tests.html'>RCPTT Report</a>";
+		tasks.appendChild(div);
+	}
 }
+
+
