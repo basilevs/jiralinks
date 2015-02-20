e50a97bd9eb765f16c6eb3c147090ee3bff680de
Vasili Gulevich <V.V.Gulevich@mail.ru>
2015-02-20 23:08:33 +0600

    More jobs are supported

--- test_results.user.js
+++ test_results.user.js
@@ -3,16 +3,28 @@
 // @namespace   basilevs
 // @include     https://hudson.eclipse.org/rcptt*/job/rcptt-all-gerrit/*
 // @include     https://hudson.eclipse.org/rcptt*/job/rcptt-tests/*
+// @include     https://hudson.eclipse.org/rcptt*/job/mockups-tests/*
 // @require		https://github.com/basilevs/jiralinks/raw/master/utils.js	
-// @version     3
+// @version     4
 // @grant       GM_log
 // ==/UserScript==
 
+var jobToTestPath = {
+	"rcptt-tests": "rcpttTests",
+	"rcptt-all-gerrit": "rcpttTests",
+	"mockups-tests": "tests"
+};
+
 
 var link = getNode("(id('left-top-nav')/a)[last()]");
+var jobName = getNode("(id('left-top-nav')/a)[last()-1]").textContent;
 
 var buildNum = parseInt(link.textContent.replace(/#/g, ""), 10);
-if (!isNaN(buildNum)) {
+var path = jobToTestPath[jobName];
+GM_log("No path for job " + jobName + ": " + path);
+GM_log("Build number: " + buildNum);
+
+if (!isNaN(buildNum) && path) {
 	var inProgress = getNodes("//table[contains(@class, 'progress-bar')]").length != 0;
 	var segment = ""+buildNum;
 	var url = link.href.replace(/\/(lastBuild|\d+)\/$/, "/"+segment+"/");
@@ -22,6 +34,7 @@ if (!isNaN(buildNum)) {
 	} else {
 		url += "artifact/";
 	}
+	url += path + "/";
 	var tasks = document.getElementById('tasks');
 
 	function appendDiv(innerHtml) {
@@ -30,8 +43,8 @@ if (!isNaN(buildNum)) {
 		tasks.appendChild(div);
 	}
 	if (!inProgress)
-		appendDiv("<img style='margin: 2px;' src='/rcptt/static/f580e051/images/24x24/clipboard.png'> <a href='" + url +"rcpttTests/target/results/tests.html'>RCPTT Report</a>");
-	appendDiv("<img style='margin: 2px;' src='/rcptt/static/f580e051/images/24x24/terminal.png'> <a href='" + url +"rcpttTests/target/results/aut-console-0_console.log'>AUT консоль</a>");
+		appendDiv("<img style='margin: 2px;' src='/rcptt/static/f580e051/images/24x24/clipboard.png'> <a href='" + url +"target/results/tests.html'>RCPTT Report</a>");
+	appendDiv("<img style='margin: 2px;' src='/rcptt/static/f580e051/images/24x24/terminal.png'> <a href='" + url +"target/results/aut-console-0_console.log'>AUT консоль</a>");
 }
 
 
